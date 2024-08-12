9e528c9b6df339e69d3691fd0af822b0247b8a05
Romain Lambert <rolamber@z3r4p6.42lyon.fr>
2024-08-12 10:53:07 +0200

    starting  parsing routine

--- parsing_routine.c
+++ parsing_routine.c
@@ -1,14 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parsing_routine.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rolamber <rolamber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/12 08:24:17 by rolamber          #+#    #+#             */
-/*   Updated: 2024/08/12 10:40:06 by rolamber         ###   ########.fr       */
+/*   Created: 2024/08/12 10:42:22 by rolamber          #+#    #+#             */
+/*   Updated: 2024/08/12 10:52:46 by rolamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "headers/cub3d.h"
 
+int	parsing(char *path, t_game *game)
+{
+
+}
+
+int	get_texture_information(t_map *map)
+{
+
+}
+
+int	get_map_information(t_map *map)
+{
+
+}
+
+int init_map(t_map *map)
+{
+	
+}
+
+int	check_texture_information(t_map *map)
+{
+
+}
+
+int	check_map_information(t_map *map)
+{
+
+}
