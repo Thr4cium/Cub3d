b04dfadafb2635546d5b3adc416903090624503c
Romain Lambert <rolamber@z3r2p4.42lyon.fr>
2024-08-19 15:41:26 +0200

    rgb to int and stuff

--- parsing_routine.c
+++ parsing_routine.c
@@ -6,7 +6,7 @@
 /*   By: rolamber <rolamber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 10:42:22 by rolamber          #+#    #+#             */
-/*   Updated: 2024/08/13 14:37:27 by rolamber         ###   ########.fr       */
+/*   Updated: 2024/08/19 15:15:22 by rolamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,10 @@ int get_information(char *path, t_map *map)
 		return (perror("open"), -1);
 	if (get_texture_information(map, fd) == -1)
 		return (-1);
+	if (check_texture_information(map) == -1)
+		return (-1);
+	if (get_map_information(map) == -1)
+		return (-1);
 }
 
 int	get_texture_information(t_map *map, int fd)
@@ -53,9 +57,10 @@ int	get_texture_information(t_map *map, int fd)
 		else if (ft_strncmp(line, "F ", 2))
 			map->ground_color = rgb_to_int(line);
 		else if (!is_line_only_empty(line) && !is_line_is_map(line))
-			return (printf("Error : wrong texture information\n"), -1);
+			return (free(line), printf("Error : wrong texture information\n"), -1);
 		if (are_texture_filled(map))
-			return (0);
+			return (free(line), 0);
+		free(line);
 		line = get_next_line(fd);
 	}
 	return (printf("Error : no texture informations found\n"), -1);
@@ -79,6 +84,8 @@ t_map   *init_map(void)
 	map->so_texture = NULL;
 	map->we_texture = NULL;
 	map->ea_texture = NULL;
+	map->ground_color = 0;
+	map->sky_color = 0;
 	map->map = NULL;
 	map->map_x = 0;
 	map->map_y = 0;
@@ -86,9 +93,35 @@ t_map   *init_map(void)
 
 int	check_texture_information(t_map *map)
 {
+	int fd;
 
+	fd = open(map->no_texture, O_RDONLY);
+	if (fd == -1)
+		return (perror("open"), -1);
+	close(fd);
+	fd = open(map->so_texture, O_RDONLY);
+	if (fd == -1)
+		return (perror("open"), -1);
+	close(fd);
+	fd = open(map->we_texture, O_RDONLY);
+	if (fd == -1)
+		return (perror("open"), -1);
+	close(fd);
+	fd = open(map->ea_texture, O_RDONLY);
+	if (fd == -1)
+		return (perror("open"), -1);
+	close(fd);
+	if (check_color_information(map) == -1)
+		return (-1);
+	return (0);
 }
 
+int	check_color_information(t_map *map)
+{
+	if (map->ground_color < 0 || map->sky_color < 0)
+		return (printf("Error : wrong color information\n"), -1);	
+	return (0);
+}
 int	check_map_information(t_map *map)
 {
 
@@ -129,5 +162,42 @@ bool	is_line_is_map(char *line)
 
 int	rgb_to_int(char *line)
 {
+	char color[4] = {0};
+	int rgb;
+	int i;
+	int j;
 
+	while (*line == ' ')
+		line++;
+	i = 0;
+	j = 0;
+	rgb = 0;
+	while (*line && j < 3)
+	{
+		while (*line != ',' && i  < 3)
+		{
+			if (!ft_isdigit(*line))
+				return (printf("Error : wrong color information\n"), -1);	
+			color[i++] = *line;
+		}
+		rgb = rgb_to_int_tool(j, color, rgb);
+		ft_bzero(color, 4);
+		i = 0;
+		j++;
+		line++;
+	}
+	return (rgb);
+}
+
+int rgb_to_int_tool(int j, char *color, int rgb)
+{
+	if (!color)
+		return (printf("Error : wrong color information\n"), -1);
+	if (j == 0)
+		rgb = ft_atoi(color) << 16;
+	else if (j == 1)
+		rgb += ft_atoi(color) << 8;
+	else if (j == 2)
+		rgb += ft_atoi(color);
+	return (rgb);
 }
