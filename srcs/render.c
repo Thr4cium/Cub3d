ec6f1836aa926ffaa8f45ddb6157de57c8198eb7
Romain Lambert <rolamber@z3r2p5.42lyon.fr>
2024-09-26 11:26:57 +0200

    fixing segfault when too close from wall

--- srcs/render.c
+++ srcs/render.c
@@ -6,7 +6,7 @@
 /*   By: rolamber <rolamber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 08:32:51 by rolamber          #+#    #+#             */
-/*   Updated: 2024/09/26 10:13:32 by rolamber         ###   ########.fr       */
+/*   Updated: 2024/09/26 11:25:04 by rolamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,15 @@ bool	is_there_wall(t_game *game, t_move move)
 	int	x;
 	int	y;
 
-	x = game->pos_x + game->dir_x * 0.1;
-	y = game->pos_y + game->dir_y * 0.1;
 	if (move == UP)
 	{
-		x = game->pos_x + game->dir_x * 0.1;
-		y = game->pos_y + game->dir_y * 0.1;
+		x = game->pos_x + game->dir_x * 0.2;
+		y = game->pos_y + game->dir_y * 0.2;
 	}
 	if (move == DOWN)
 	{
-		x = game->pos_x - game->dir_x * 0.1;
-		y = game->pos_y - game->dir_y * 0.1;
+		x = game->pos_x - game->dir_x * 0.2;
+		y = game->pos_y - game->dir_y * 0.2;
 	}
 	if (game->map->map[(int)y][(int)x] == '1')
 		return (true);
