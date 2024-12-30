3d9d98e47238a68608ad29de07ee0dc3e79e2b6b
MaxHautMax <mgrondinp@gmail.com>
2024-12-30 15:44:40 +0100

    rmv function prototype in header

--- libft/gnl/get_next_line.h
+++ libft/gnl/get_next_line.h
@@ -21,10 +21,7 @@
 # endif
 
 char	*get_next_line(int fd);
-char	*ft_read_file(int fd, char *buffer);
 char	*ft_strjoin_gnl(char *s1, char *s2);
-char	*ft_trim_buffer(char *buffer);
-char	*ft_cpy_current_line(char *buffer);
 void	ft_reset_malloc(void *ptr);
 
 #endif
